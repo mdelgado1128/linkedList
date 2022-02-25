acc18398b97aa1ea60c050329c5da6426c4e7a1c
Mark Delgado <markdelgado94@gmail.com>
2022-02-25 06:43:44 -0800

    A linked list with 3 nodes

---
+++ linkedListExample.cpp
@@ -0,0 +1,36 @@
+#include <iterator>
+#include <iostream>
+#
+#include <list>
+
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+};
+
+int main()
+{
+
+  Node *head = NULL;
+  Node *second = NULL;
+  Node *third = NULL;
+
+  head = new Node();
+  second = new Node();
+  third = new Node();
+
+  head->data = 1;
+  head->next = second;
+
+  second->data = 2;
+  second->next = third;
+
+  third->data = 3;
+  third->next = NULL;
+
+  return 0;
+}
